ff19f1b7508d89fde77cae4482a36b64fa0ed417
Guilherme Viana <guilhermevianasiqueiracim@gmail.com>
2026-08-10 20:44:21 -0300

    Add files via upload

---
+++ NetBeansProjects/maven_project_java2/src/main/java/com/mycompany/maven_project_java2/Maven_project_java2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.maven_project_java2;
+
+/**
+ *
+ * @author Admin
+ */
+public class Maven_project_java2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
